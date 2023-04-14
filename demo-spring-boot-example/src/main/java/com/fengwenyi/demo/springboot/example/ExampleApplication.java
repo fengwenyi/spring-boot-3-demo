48fee3df503e400508c7606a5561d6429d513c64
ErwinFeng <xfsy_2015@163.com>
2023-04-14 10:45:50 +0800

    example application

--- demo-spring-boot-example/src/main/java/com/fengwenyi/demo/springboot/example/ExampleApplication.java
+++ demo-spring-boot-example/src/main/java/com/fengwenyi/demo/springboot/example/ExampleApplication.java
@@ -1,5 +1,8 @@
 package com.fengwenyi.demo.springboot.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author <a href="https://fengwenyi.com">Erwin Feng</a>
  * @since 2023-04-14
