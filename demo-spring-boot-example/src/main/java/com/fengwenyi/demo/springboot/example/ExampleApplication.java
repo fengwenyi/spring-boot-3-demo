f50229b15900cc24090740794ec9c8c5b40c2bbe
ErwinFeng <xfsy_2015@163.com>
2023-04-14 10:45:15 +0800

    first commit

---
+++ demo-spring-boot-example/src/main/java/com/fengwenyi/demo/springboot/example/ExampleApplication.java
@@ -0,0 +1,14 @@
+package com.fengwenyi.demo.springboot.example;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2023-04-14
+ */
+@SpringBootApplication
+public class ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+
+}
---
+++ demo-spring-boot-example/src/test/java/com/fengwenyi/demo/springboot/example/ExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fengwenyi.demo.springboot.example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author <a href="https://fengwenyi.com">Erwin Feng</a>
+ * @since 2023-04-14
+ */
+@SpringBootTest
+public class ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
